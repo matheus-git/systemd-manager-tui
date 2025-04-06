a4c657cf2e9ac1c66f189812ebeac3fa061f3338
Matheus-git <mathiew0@gmail.com>
2025-04-06 18:55:55 -0300

    feat: check if has permissions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use libc;
+
 mod infrastructure;
 mod domain;
 mod usecases;
@@ -5,6 +7,10 @@ mod terminal;
 use terminal::terminal::App;
 
 fn main() -> color_eyre::Result<()> {
+    if unsafe { libc::geteuid() } != 0 {
+        eprintln!("❌ This application must be run with sudo (as root).");
+        std::process::exit(1);
+    }
     color_eyre::install()?;
     let terminal = ratatui::init();
     let result = App::new().run(terminal);
