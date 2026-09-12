ef6d3dc564a893100183287e2c485370f6514369
Matheus-git <mathiew0@gmail.com>
2026-09-11 21:50:08 -0300

    Revert "feat: support instantiated systemd services"
    This reverts commit 48f9ea43f227ed3326771f8833d2123cb9c0d58c.

--- src/terminal/components/list.rs
+++ src/terminal/components/list.rs
@@ -177,21 +177,6 @@ pub enum ServiceAction {
     ToggleMask,
 }
 
-impl ServiceAction {
-    pub fn label(self) -> &'static str {
-        match self {
-            Self::Start => "start",
-            Self::Stop => "stop",
-            Self::Restart => "restart",
-            Self::Enable => "enable",
-            Self::Disable => "disable",
-            Self::ToggleMask => "mask/unmask",
-            Self::RefreshAll => "refresh",
-            Self::ToggleFilter => "toggle filter",
-        }
-    }
-}
-
 pub enum QueryUnitFile {
     Finished(HashMap<String, String>),
     Error(String),
